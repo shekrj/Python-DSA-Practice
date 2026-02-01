17e53e1747b9051a1f50aa44251b2727ec4c6194
Abhishek Kumar <harryabhishek2007@gmail.com>
2026-02-02 00:31:30 +0530

    Delete

--- Step_8 - Bit Manipulation/14_checkOdd.py
+++
@@ -1,6 +0,0 @@
-#check if a number is odd or not:
-def func(n):
-    if n&(1)==1:
-        return "Odd"
-    else:
-        return "Even"
--- Step_8 - Bit Manipulation/9_minBitFlips.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def minBitFlips(self, start, goal):
-        xor = start ^ goal
-        count = 0
-        while xor:
-            if xor & 1:
-                count += 1
-            xor >>= 1
-        return count
