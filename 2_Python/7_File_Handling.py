d84aafeaae261f6edbb3bec05f38b14572644017
Abhishek Kumar <harryabhishek2007@gmail.com>
2026-02-02 00:42:48 +0530

    Delete

--- 2_Python/7_File_Handling.py
+++
@@ -1,9 +0,0 @@
-try:
-    with open('File.txt', 'r') as f:
-        data=f.read()
-        print(data)
-except Exception as e:
-    print(f'Error: {e}')
-finally:
-    print("File operation completed")
-
